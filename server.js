25f217ef2f8b082a5434765146613330ff02c474
willisrc <william.r.cronin@gmail.com>
2026-03-08 22:24:50 -0400

    Fix static path and use sendFile for home

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
 var port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get("/", function(req, res) {
-    res.render("home");
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
 
 app.listen(port, function() {
-    console.log("Server is running on port " + port);
+    console.log('Server is running on port ' + port);
 });
